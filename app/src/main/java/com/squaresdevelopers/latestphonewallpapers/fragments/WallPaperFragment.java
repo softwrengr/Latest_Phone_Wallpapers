3e7cb24fe5731c9e3f77f424d78ed15f69375b59
Attaurrahman <attaurrahman321@gmail.com>
2018-12-04 00:18:36 +0500

    delete

--- app/src/main/java/com/squaresdevelopers/latestphonewallpapers/fragments/WallPaperFragment.java
+++ app/src/main/java/com/squaresdevelopers/latestphonewallpapers/fragments/WallPaperFragment.java
@@ -177,18 +177,9 @@ public class WallPaperFragment extends Fragment {
                     //   Log.i(tag, "onKey Back listener is working!!!");
                     getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
 
-                    Toast.makeText(getActivity(), "back", Toast.LENGTH_SHORT).show();
-
-                    File dir = new File(Environment.getExternalStorageDirectory()+"/.HD Wallpaper");
-                    if (dir.isDirectory())
-                    {
-                        String[] children = dir.list();
-                        for (int i = 0; i < children.length; i++)
-                        {
-                            new File(dir, children[i]).delete();
-                        }
-                    }
-
+                    File filepath = Environment.getExternalStorageDirectory();
+                    File dir = new File(filepath.getAbsolutePath() + "/.HD Wallpaper");
+                   deleteDir(dir);
                     return true;
                 }
                 return false;
@@ -196,8 +187,22 @@ public class WallPaperFragment extends Fragment {
         });
 
     }
-    
-    
+
+
+    public static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i=0; i<children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+
+        // The directory is now empty so delete it
+        return dir.delete();
+    }
 
 }
 
